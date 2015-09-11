7a9f8f1ec5b2a0c9c8b881c0e96af12ea2741e93
Paul Elliott <paul@codingfrontier.com>
2015-09-11 17:25:20 -0400

    Update the comments in the strings section

--- test/2-strings.js
+++ test/2-strings.js
@@ -8,29 +8,38 @@
  *   - endsWith
  *   - includes
  *   - repeat
- *   - trim
+ *
+ * Further Reading:
  *
  * https://babeljs.io/docs/learn-es2015/#template-strings
+ *
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String#Template_strings
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/startsWith
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/endsWith
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/includes
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat
  */
 const assert = require("assert");
 
 describe('Section 2 - Strings', function() {
   describe('template strings', function () {
     it('can work just like normal strings', function () {
-      const x = ``; // update the value of `x` here
+      // TODO: update the value of `x` here
+      const x = ``;
 
       assert.equal(x, 'initial value', 'Update the value of `x` to be "initial value"');
     });
 
     it('can accept embedded variables', function() {
       const y = "value";
-      const x = `initial `; // Add interpolation for "y" here
+      // TODO: Interpolate the value of y into x
+      const x = `initial `;
 
       assert.equal(x, 'initial value', `Use interpolation to embed "${y}"`);
     });
 
     it('can span multiple lines', function() {
+      // TODO: Update this string to match the expectation
       const x = `
       `;
 
@@ -42,6 +51,7 @@ describe('Section 2 - Strings', function() {
 
   describe('#startsWith', function() {
     it('returns true if the string begins with the test string', function() {
+      // TODO: Change initial to updated to make the expectation pass
       const x = 'initial value';
 
       assert.ok(x.startsWith('updated'), 'Change "initial" to "updated" in "x"');
@@ -50,6 +60,7 @@ describe('Section 2 - Strings', function() {
 
   describe('#endsWith', function() {
     it('returns true if the string ends with the test string', function() {
+      // TODO: Append value to this string to make the expectation pass
       const x = 'initial ';
 
       assert.ok(x.endsWith('value'), 'Append "value" to "x"');
@@ -58,6 +69,7 @@ describe('Section 2 - Strings', function() {
 
   describe('#includes', function() {
     it('returns true if the string includes the test string', function() {
+      // TODO: Fix the spelling of initial here to make the expectation pass
       const x = 'inital value';
 
       assert.ok(x.includes('tial'), 'Fix the spelling of "initial" in "x"');
@@ -66,17 +78,10 @@ describe('Section 2 - Strings', function() {
 
   describe('#repeat', function() {
     it('returns the string repeated some number of times', function() {
+      // TODO: Change the value passed to repeat here to make the expectation pass
       const x = 'foobar'.repeat(1);
 
       assert.equal(x, 'foobarfoobarfoobar', 'Change the number of repeats');
-    });
-  });
-
-  describe('#trim', function() {
-    it('trims leading and trailing whitespace from the string', function() {
-      const x = '    foobar    ';
-
-      assert.equal(x, 'foobar', 'Add `.trim()` to "x"');
     });
   });
 });
