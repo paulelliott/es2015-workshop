88bc8b8e555278477fb992337c806f23a5772065
Paul Elliott <paul@codingfrontier.com>
2015-09-14 12:31:36 -0400

    Solutions to section 9

--- test/9-new-built-in-classes.js
+++ test/9-new-built-in-classes.js
@@ -21,8 +21,7 @@ describe('Section 9 - New Built-In Classes', function() {
     it('prevents duplicates', function () {
       const set = new Set();
 
-      // TODO: Call the `add` function on the set and pass it values to make the test pass.
-      // Make sure you try adding multiple of the same value to see them automatically stripped out.
+      set.add(1).add(1).add(2).add(3);
 
       assert.deepEqual([...set], [1, 2, 3], 'Add items to the set to match the assertion');
     });
@@ -30,8 +29,7 @@ describe('Section 9 - New Built-In Classes', function() {
     it('can be initialized with an enumerable', function () {
       const array = [1, 1, 2, 2, 3, 3];
 
-      // TODO: Pass the array into the set initializer.
-      const set = new Set();
+      const set = new Set(array);
 
       assert.deepEqual(new Array(...set), [1, 2, 3], 'Initialize `set` with the array');
     });
@@ -39,8 +37,10 @@ describe('Section 9 - New Built-In Classes', function() {
 
   describe('Map', function () {
     it('can be initialized with a key/value array', function () {
-      // TODO: Pass a two-dimensional array into the initializer with the expected values.
-      const map = new Map();
+      const map = new Map([
+        [1, 'a'],
+        ['b', 2]
+      ]);
 
       assert.equal(map.get(1), 'a', "Pass an array to the initializer with `[1, 'a']` as the first element");
       assert.equal(map.get('b'), 2, "Add `['b', 2]` as another element to the initializing array");
@@ -57,7 +57,9 @@ describe('Section 9 - New Built-In Classes', function() {
       // Store the raw data from the Map here to verify.
       const keysAndValues = [];
 
-      // TODO: Iterate over the map with a for...of loop.
+      for (const [key, value] of map) {
+        keysAndValues.push([key, value]);
+      }
 
       assert.deepEqual(keysAndValues, [
         ['one', 'first'],
@@ -75,8 +77,7 @@ describe('Section 9 - New Built-In Classes', function() {
         three: 'third'
       };
 
-      // TODO: Call `Reflect.ownKeys(o)` to get the properties of the object
-      const keys = [];
+      const keys = Reflect.ownKeys(o);
 
       assert.deepEqual(keys, ['one', 'two', 'three'], 'Add properties to the object to match the expectation');
     });
@@ -88,8 +89,8 @@ describe('Section 9 - New Built-In Classes', function() {
       };
 
       // TODO: Use `Reflect.has` to determine if these properties exist
-      const hasOne = false;
-      const hasTwo = false;
+      const hasOne = Reflect.has(o, 'one');
+      const hasTwo = Reflect.has(o, 'two');
       const hasThree = Reflect.has(o, 'three');
 
       assert.ok(hasOne, "Call `Reflect.has(o, 'one')` to determine if the property exists or not");
